cf03293aa04a562ebf6868b0c573aefc5739bcd6
Shashank <codebreaker379@gmail.com>
2023-10-08 05:08:41 +0530

    Implemented rate limting

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express=require("express");
 const morgan=require("morgan");
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const { rateLimit } = require('express-rate-limit')
 const app=express();
-app.use(morgan("combined"))
 PORT=3005;
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	max:5
+});
+app.use(morgan("combined"));
+app.use(limiter)
+
+app.use("/bookingservice",(req,res,next)=>{
+    console.log("Hit Another Service");
+    next();
+})
 app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3002/',changeOrigin:true}));
 app.get("/home",(req,res)=>{
     return res.json({
