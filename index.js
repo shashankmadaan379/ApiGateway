b2af44c859262e2e81164d0fe4d269ad6e51b7d9
Shashank <codebreaker379@gmail.com>
2023-10-07 16:45:44 +0530

    Implemented Reverse proxy.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express=require("express");
 const morgan=require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app=express();
 app.use(morgan("combined"))
 PORT=3005;
+app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3002/',changeOrigin:true}));
 app.get("/home",(req,res)=>{
     return res.json({
         message:"Hello World"
